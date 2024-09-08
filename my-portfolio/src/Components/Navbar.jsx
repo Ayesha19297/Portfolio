ba5c6e2f28ba95869eed53abfde3a7aa9b842e04
ayesha19297 <ayesha19297@gmail.com>
2024-09-09 00:40:06 +0530

    update animation

--- my-portfolio/src/Components/Navbar.jsx
+++ my-portfolio/src/Components/Navbar.jsx
@@ -1,20 +1,18 @@
 import logo from "../assets/A-logo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 
 const Navbar = () => {
   return (
     <nav className="mb-4 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img
-          className="mx-2 w-10"
-          src={logo}
-          alt="logo"
-        ></img>
+        <img className="mx-2 w-10" src={logo} alt="logo"></img>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
         <FaGithub />
+        <SiGmail />
       </div>
     </nav>
   );
