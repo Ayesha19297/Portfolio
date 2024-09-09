4209c7c35f51d8b40fdf77c8e67dc29361cb38ed
ayesha19297 <ayesha19297@gmail.com>
2024-09-09 23:23:49 +0530

    added links

--- my-portfolio/src/Components/Navbar.jsx
+++ my-portfolio/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/A-logo.png";
+import { TbHexagonLetterA } from "react-icons/tb";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
@@ -6,13 +6,31 @@ import { SiGmail } from "react-icons/si";
 const Navbar = () => {
   return (
     <nav className="mb-4 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo"></img>
+      <div className="flex text-4xl">
+        <TbHexagonLetterA />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <SiGmail />
+        <a
+          href="https://www.linkedin.com/in/ayesha19297"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://www.github.com/Ayesha19297"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="mailto:ayesha19297@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SiGmail />
+        </a>
       </div>
     </nav>
   );
