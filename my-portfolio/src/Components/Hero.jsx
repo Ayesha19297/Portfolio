9c630e8f508b0dd662bb4bd55d934738741c19cf
ayesha19297 <ayesha19297@gmail.com>
2024-09-11 00:22:07 +0530

    ui changes

--- my-portfolio/src/Components/Hero.jsx
+++ my-portfolio/src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profile from "../assets/me_working.jpg";
+//import profile from "../assets/me_working.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
@@ -21,7 +21,7 @@ const Hero = () => {
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl bg-gradient-to-r from-pink-600 via-slate-600 to-purple-600 bg-clip-text text-transparent"
+              className="pb-16 text-8xl font-thin tracking-tight lg:mt-16 lg:text-10xl bg-gradient-to-r from-pink-600 via-slate-600 to-purple-600 bg-clip-text text-transparent"
             >
               Ayesha.
             </motion.h1>
@@ -29,30 +29,30 @@ const Hero = () => {
               variants={container(0.5)}
               initial="hidden"
               animate="visible"
-              className="bg-gradient-to-r from-pink-500 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
+              className="bg-gradient-to-r from-pink-500 via-slate-500 to-purple-500 bg-clip-text text-6xl tracking-tight text-transparent"
             >
               Frontend developer
             </motion.span>
-            <motion.p
-              variants={container(1)}
-              initial="hidden"
-              animate="visible"
-              className="my-2 max-w-xl py-6 font-light tracking-tighter"
-            >
-              {HERO_CONTENT}
-            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <motion.img
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-medium text-xl text-justify tracking-tighter"
+            >
+              {HERO_CONTENT}
+            </motion.p>
+            {/* <motion.img
               initial={{ x: 100, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
               transition={{ duration: 1, delay: 1.2 }}
               className="rounded-full h-80 w-80"
               src={profile}
               alt="Ayesha"
-            />
+            /> */}
           </div>
         </div>
       </div>
--- my-portfolio/src/Components/Projects.jsx
+++ my-portfolio/src/Components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 1, y: -100 }}
         transition={{ duration: 0.5 }}
-        className="my-10 text-center text-5xl"
+        className="my-20 text-center text-5xl"
       >
         Projects
       </motion.h1>
